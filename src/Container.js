16610fe96a230cd7f3de1375c280d4d314b94fe0
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-22 15:50:10 -0500

    render list of people from MOCK_DATA

--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MOCK_DATA from './MOCK_DATA.json';
 
 const Container = () => {
+  const [peopleList] = useState(MOCK_DATA);
   return (
     <div className="Container">
       <h1 className="Container-header">The Person Finder</h1>
@@ -16,6 +18,9 @@ const Container = () => {
         onChange={(e) => console.log(e.target.value)}
         placeholder="Type a name..."
       ></input>
+      {peopleList.map((person) => (
+        <h5>{person.name}</h5>
+      ))}
     </div>
   );
 };
