978f3bce9e091d9dfd91a9a8c8b8ebd85680150e
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-22 17:53:34 -0500

    spelling error fixed

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -5,7 +5,7 @@ const PersonCard = (props) => {
   return (
     <div className="PersonCard">
       <img src={props.avatar} alt={`${props.name} Avatar`} />
-      <div classMame="PersonCard-info">
+      <div className="PersonCard-info">
         <h3>{props.name}</h3>
         <p>{props.description}</p>
       </div>
