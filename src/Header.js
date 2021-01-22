4bba97b425a39377ad37670f55a730e428d2f3c3
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-22 15:43:01 -0500

    Create Header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <p className="logo">logo</p>
+    </div>
+  );
+};
+
+export default Header;
