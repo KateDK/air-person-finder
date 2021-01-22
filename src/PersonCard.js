22fb06b114a545caf7516e62a0ee32cf88b7cf41
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-22 16:18:30 -0500

    Add PersonCard CSS

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './PersonCard.css';
 
 const PersonCard = (props) => {
   return (
     <div className="PersonCard">
       <img src={props.avatar} alt={`${props.name} Avatar`} />
-      <div>
+      <div classMame="PersonCard-info">
         <h3>{props.name}</h3>
         <p>{props.description}</p>
       </div>
