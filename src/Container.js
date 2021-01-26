c8e65317a5f290e42ec82772284550f5e2dcd951
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-26 16:08:25 -0500

    Add infinite scrolling

--- src/Container.js
+++ src/Container.js
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import PersonCard from './PersonCard';
 import MOCK_DATA from './MOCK_DATA.json';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './Container.css';
 
 const Container = () => {
   const [peopleList] = useState(MOCK_DATA);
   const [value, setValue] = useState('');
-
+  const [count, setCount] = useState({
+    prev: 0,
+    next: 10,
+  });
+  const [hasMore, setHasMore] = useState(true);
+  const [current, setCurrent] = useState(
+    peopleList.slice(count.prev, count.next)
+  );
+  const getMoreData = () => {
+    if (current.length === peopleList.length) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setCurrent(
+        current.concat(peopleList.slice(count.prev + 10, count.next + 10))
+      );
+    }, 20);
+    setCount((prevState) => ({
+      prev: prevState.prev + 10,
+      next: prevState.next + 10,
+    }));
+  };
   const filterLogic = (person) => {
     const lowercaseName = person.name.toLowerCase();
     const lowerCaseVal = value.toLocaleLowerCase();
     return lowercaseName.includes(lowerCaseVal);
   };
+
   return (
     <div className="Container">
       <h1 className="Container-header">The Person Finder</h1>
@@ -30,17 +54,23 @@ const Container = () => {
         onChange={(e) => setValue(e.target.value)}
         placeholder="Type a name..."
       ></input>
-
-      {peopleList
-        .filter((person) => filterLogic(person))
-        .map((person) => (
-          <PersonCard
-            name={person.name}
-            avatar={person.avatar}
-            description={person.description}
-            key={person.id}
-          />
-        ))}
+      <InfiniteScroll
+        dataLength={current.length}
+        next={getMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        {peopleList
+          .filter((person) => filterLogic(person))
+          .map((person) => (
+            <PersonCard
+              name={person.name}
+              avatar={person.avatar}
+              description={person.description}
+              key={person.id}
+            />
+          ))}
+      </InfiniteScroll>
     </div>
   );
 };
